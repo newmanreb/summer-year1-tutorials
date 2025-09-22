c48bbadfc53448f03b679140d0e43796ab2280c9
newmanreb <rebekah.s.newman@gmail.com>
2025-09-22 15:48:27 +0100

    Updated requirements.txt, added test.py scripts for each module

---
+++ tests/test_transcribe_DNA_to_RNA.py
@@ -0,0 +1,7 @@
+import pytest
+from summer_year1_tutorials.modules import chunk_text
+
+# -----------------------------------------------------
+# Tests for chunk_text()
+# -----------------------------------------------------
+
