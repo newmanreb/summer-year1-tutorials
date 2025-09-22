b4eba02cdff70c402a70e8fafdcd1b0c91e12a64
newmanreb <rebekah.s.newman@gmail.com>
2025-09-22 14:23:05 +0100

    Updated docstrings for all functions.

--- summer_year1_tutorials/modules/chunk_text.py
+++ summer_year1_tutorials/modules/chunk_text.py
@@ -1,20 +1,61 @@
-## 1.4
-# Given a string representing a DNA sequence, print in blocks, with gaps every so many spaces
-# Given the following sequence print with block size 10:
-sequence = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
-block_size = 10 # length of the blocks we want
-sequence_length = len(sequence) # get the length of the input sequence
-index = 0   # start index at 0
+# chunk_text.py
+# Import modules
+from summer_year1_tutorials.utils.dna_integrity_check import dna_integrity_check
+from summer_year1_tutorials.logger import logger
 
-while index < sequence_length:  # loop through the length of the sequence
-    block = sequence[index:index + block_size]  # extract a block of characters by block_size
-    print(block)    # print that block
-    index += block_size # increase the index by the block size to keep going
+def chunk_text(sequence, block_size, seq_type="DNA"):
+    """
+    Splits a DNA sequence into evenly sized blocks and formats them into a list of chunked sequences.
 
-# Another way
-sequence = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTGAT"
-block_size = 10
-sequence_length = len(sequence)
+    Parameters
+    ----------
+    sequence: str
+        Sequence to chunk.
+    block_size: int
+        The length of each block/chunk.
+    seq_type: str
+        Input type. If "DNA", an integrity check is performed on the input sequence.
 
-for i in range(0, len(sequence), block_size):
-    print(sequence[i:i + block_size])
+    Returns
+    ----------
+    str
+        Chunked sequence by block_size.
+
+    Raises
+    ----------
+    TypeError
+        If the input type is not a string or if block_size is not an integer.
+    ValueError
+        If seq_type is "DNA" and the sequence contains uracil (U).
+        If seq_type is "DNA" and the sequence contains invalid bases (anything other than A, T, G, or C).
+    """
+    sequence = "".join(sequence.split()).upper()        # Remove whitespace and force uppercase on input.
+
+    if not sequence.isalpha():                          # Check for non-alpha characters in input.
+        logger.warning("Sequence contains non-alphabetic characters. Check input for digits or symbols.")
+
+    if seq_type == "DNA":                               # If input type is DNA, run integrity check.
+        dna_integrity_check(sequence)                   # Ensure all ATGC characters in DNA sequence.
+        logger.info(f"Input DNA sequence passes integrity check.")
+    else:                                               # Print user-provided sequence type to the log.
+        logger.info(f"Sequence type provided: {seq_type}")
+
+    logger.info("Chunking sequence '{}' into blocks of {}".format(sequence, block_size))
+
+    index = 0                                           # Start index at 0
+    sequence_length = len(sequence)                     # Get the length of the input sequence
+
+    try:
+        while index < sequence_length:                  # Loop through the length of the sequence
+            block = sequence[index:index + block_size]  # Extract a block of characters by block_size
+            print(block)                                # Print that block
+            index += block_size                         # Increase the index by the block size to keep going
+    except TypeError as e:
+        logger.error("Chunking failed with exception: {}".format(e))
+        raise
+
+    return index
+
+sequence1 = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTACG"
+sequence2 = "GCUGAGACUUCCUGGACGGGGGACAGGCUGUGGGGUUUCUACG"
+chunk_text(sequence2, 10, "RNA")
--- summer_year1_tutorials/modules/genbank_style.py
+++ summer_year1_tutorials/modules/genbank_style.py
@@ -1,22 +1,67 @@
-# 2.1: Refer back to exercise 1.4, where we printed a DNA string in blocks, with a space between each
-# block. Now further develop your code so that it displays a DNA string in the style used in GenBank
-# records. So given the DNA sequence:
+# genbank_style.py
+# Import modules
+from summer_year1_tutorials.utils.dna_integrity_check import dna_integrity_check
+from summer_year1_tutorials.logger import logger
 
-sequence = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTAGTCAACTTGTTGAAGAGCTATTGAAAATCATTTGTGCTTTTCAGCTTGACACAGGTTTGGAGTATGCAAACAGCTATAATTTTGCAAAAAAGGAAAATAACTCTCCTGAACATCTAAAAGATGAAGTTTCTATCATCCAAAGTATGGGCTACAGAAACCGTGCCAAAAGACTTCTACAGAGTGAACCCGAAAATCCTTCCTTGCAGGAAACCAGTCTCAGTGTCCAACTCTCTAACCTTGGAACTGTGAGAACTCTGAGGACAAAGCAGCGGATACAACCTCAAAAGACGTCTGTCTACATTGAATTGGGATCTGATTCTTCTGAAGATACCGTTAATAAGGCAACTTATTGCAGTGTGGGAGATCAAG"
+def genbank_format(sequence, block_size=10, blocks_per_row=6, seq_type="DNA"):
+    """
+    Displays an input string in GenBank format, with 10 characters per block and 6 blocks per row (default).
 
-def genbank_format(sequence, block_size=10, blocks_per_row=6):
-    seq = sequence.lower()
+    Parameters
+    ----------
+    sequence: str
+        Sequence to be formatted.
+    block_size: int
+        The length of each block. Defaults to 10.
+    blocks_per_row: int
+        The number of blocks per row. Defaults to 6.
+    seq_type:
+        Input type. If "DNA", an integrity check is performed on the input sequence.
+
+    Returns
+    ----------
+    str
+        Input sequence separated by blocks and rows. Lowercase enforced.
+
+    Raises
+    ----------
+    TypeError
+        If the input type is not a string or if block_size and blocks_per_row are not integers.
+    ValueError
+        If seq_type is "DNA" and the sequence contains uracil (U).
+        If seq_type is "DNA" and the sequence contains invalid bases (anything other than A, T, G, or C).
+    """
+    sequence = "".join(sequence.split()).lower()        # Remove whitespace and force lowercase on input.
+
+    if not sequence.isalpha():                          # Check for non-alpha characters in input.
+        logger.warning("Sequence contains non-alphabetic characters. Check input for digits or symbols.")
+
+    if seq_type == "DNA":                               # If input type is DNA, run integrity check.
+        dna_integrity_check(sequence)                   # Ensure all ATGC characters in DNA sequence.
+        logger.info(f"Input DNA sequence passes integrity check.")
+    else:                                               # Print user-provided sequence type to the log.
+        logger.info(f"Sequence type provided: {seq_type}")
+
+    logger.info("Chunking sequence '{}' into blocks of {}, with {} blocks per row".format(sequence, block_size,
+                                                                                          blocks_per_row))
+
     row_size = block_size * blocks_per_row
 
-    for start in range(0, len(seq), row_size):
-        row_seq = seq[start:start + row_size]
+    try:
+        for start in range(0, len(sequence), row_size):
+            row_seq = sequence[start:start + row_size]
 
-        blocks = []
-        for i in range(0, len(row_seq), block_size):
-            block = row_seq[i:i + block_size]
-            blocks.append(block)
+            blocks = []
+            for i in range(0, len(row_seq), block_size):
+                block = row_seq[i:i + block_size]
+                blocks.append(block)
 
-        formatted_row = ' '.join(blocks)
-        print(f"{start + 1:<9}{formatted_row}")
+            formatted_row = ' '.join(blocks)
+            print(f"{start + 1:<9}{formatted_row}")
+    except TypeError as e:
+        logger.error("Chunking failed with exception: {}".format(e))
+        raise
 
-genbank_format(sequence)
+sequence = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTCTGGGTAAAGTTCATTGGAACAGAAAGAAATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGATATAACCAAAAGGAGCCTACAAGAAAGTACGAGATTTAGTCAACTTGTTGAAGAGCTATTGAAAATCATTTGTGCTTTTCAGCTTGACACAGGTTTGGAGTATGCAAACAGCTATAATTTTGCAAAAAAGGAAAATAACTCTCCTGAACATCTAAAAGATGAAGTTTCTATCATCCAAAGTATGGGCTACAGAAACCGTGCCAAAAGACTTCTACAGAGTGAACCCGAAAATCCTTCCTTGCAGGAAACCAGTCTCAGTGTCCAACTCTCTAACCTTGGAACTGTGAGAACTCTGAGGACAAAGCAGCGGATACAACCTCAAAAGACGTCTGTCTACATTGAATTGGGATCTGATTCTTCTGAAGATACCGTTAATAAGGCAACTTATTGCAGTGTGGGAGATCAAG"
+sequence2 = "GCTGAGACTTCCTGGACGGGGGACAGGCTGTGGGGTTTCTCAGATAACTGGGCCCCTGCGCTCAGGAGGCCTTCACCCTCTGCTC"
+genbank_format(sequence2)
